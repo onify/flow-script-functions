8a71ebab1af4230de2d3deb40e72a42a773218ea
KathrynSheenaMedina <kathryn.medina@mllrdev.com>
2024-02-28 01:42:50 +0800

    test: update script

--- src/slugify/slugify.test.ts
+++ src/slugify/slugify.test.ts
@@ -1,10 +1,10 @@
 import * as func from './slugify';
 
-describe('Slugify', () => {
+describe('slugify', () => {
   const text = 'Hello World!';
   const result = 'hello-world';
 
-  it(`transform ${text} to ${result}`, () => {
+  it(`transform "${text}" to "${result}"`, () => {
     expect(func.slugify(text)).toBe(result);
   });
 });
