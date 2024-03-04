786c6bcc91f09a9108ec0724114fc37815407ead
Kathryn Sheena Medina <kathryn.medina@mllrdev.com>
2024-03-05 01:08:42 +0800

    feat: create project for flow script functions (#1)

---
+++ src/slugify.ts
@@ -0,0 +1,16 @@
+export function slugify(text: string) {
+  const specialCharacters = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+  const validCharacters = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+  const pattern = new RegExp(specialCharacters.split('').join('|'), 'g');
+
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(pattern, (c) => validCharacters.charAt(specialCharacters.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+}
---
+++ test/slugify.ts
@@ -0,0 +1,18 @@
+import * as func from '../src/slugify';
+
+describe('slugify', () => {
+  const testCases = [
+    { text: 'Hello World!', expected: 'hello-world' },
+    { text: 'Google', expected: 'google' },
+    { text: 'Jag är en rejäl påg', expected: 'jag-ar-en-rejal-pag' },
+    { text: 'комплекс', expected: '' },
+    { text: 'Te$t1n£', expected: 'tet1n' },
+  ];
+
+  for (const testCase of testCases) {
+    it(`transform "${testCase.text}" to "${testCase.expected}"`, () => {
+      const output = func.slugify(testCase.text);
+      expect(output).toBe(testCase.expected);
+    });
+  }
+});
